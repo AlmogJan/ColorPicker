f4b258f8b6da6b0629aa199b48bec40615a0bb88
AlmogJan <Almogj1998@gmail.com>
2024-07-28 13:22:47 +0300

    basic-colorPicker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
+import { ColorPicker } from "./comps/colorPicker";
 
 export function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/picker")
+    fetch("https://colorpicker-qaw9.onrender.com/api/picker")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
@@ -13,6 +14,7 @@ export function App() {
     <div className="App">
       <h1>Hello pitzy24456232werwesadfadsr</h1>
       <h1>{message}</h1>
+      <ColorPicker></ColorPicker>
     </div>
   );
 }
--- frontend/src/comps/colorPicker.tsx
+++ frontend/src/comps/colorPicker.tsx
@@ -1,5 +1,10 @@
-export function colorPicker() {
+import { useState } from "react"
+
+export function ColorPicker() {
+    const [color, setColor] = useState<String>('#000000')
+
     return <div className="color-container">
-        <div className="color"></div>
+        <input type="color" value={'#000000'} onChange={(ev) => setColor(ev.target.value)} />
+        <div>{color}</div>
     </div>
 }
